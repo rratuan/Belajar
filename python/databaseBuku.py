e002c6052322b46345b3e8e935728c1807569e75
rratuan <ratua4820@gmail.com>
2023-11-24 23:16:33 +0700

    menambah file latihan database buku

--- python/databaseBuku.py
+++ python/databaseBuku.py
@@ -1,14 +1,18 @@
 print(' Database Buku '.center(40,'='))
 
-list_buku = []
+list_buku = [] #di assign di luar loop supaya data list yang sudah ada tidak teriset
 while True:
+    # menginputkan data buku
     judul = input('Judul Buku \t: ')
     penulis = input('Penulis \t: ')
 
+    # menampung data buku ke dalam list
     buku = [judul,penulis]
+    # menambahkan list data buku ke dalam list buku buku 
     list_buku.append(buku)
     # print(list_buku)
 
+    # menunjukan data list buku buku berupa tabel
     print('-'*60)
     print('|    No     |        Judul        |         Penulis       |')
     print('-'*60)
